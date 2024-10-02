fe366386b037df75ea40c91d045411346304fa89
Roman Timoshkin <rmntim@yandex.ru>
2024-10-02 15:23:36 +0300

    add points servlet

--- src/main/java/ru/rmntim/web/labwork2/models/Point.java
+++ src/main/java/ru/rmntim/web/labwork2/models/Point.java
@@ -1,6 +1,8 @@
 package ru.rmntim.web.labwork2.models;
 
-public record Point(double x, double y, double r, boolean isInside) {
+import jakarta.json.bind.annotation.JsonbTransient;
+
+public record Point(double x, double y, double r, @JsonbTransient boolean isInside) {
     public Point(double x, double y, double r) {
         this(x, y, r, isInside(x, y, r));
     }
